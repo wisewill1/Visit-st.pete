b8c9f21fb5fde1b019f1820173a1290fc2784dae
wisewill1 <wisewill1@gmail.com>
2019-10-23 22:44:37 -0400

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,19 @@
 
 				}
 
+				
 		};
+
+	function myFunction() {
+		var x = document.getElementById("myDIV");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+	}
+
+	
 
 	// Breakpoints.
 		breakpoints({
